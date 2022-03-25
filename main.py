789b94613df3f869a083da3b1a610291c4d5f89a
Qandeel90 <qandeeltariq90@gmail.com>
2022-03-25 23:42:30 +0500

    set models

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("../saved_models/1")
+MODEL = tf.keras.models.load_model("models")
 
 CLASS_NAMES = ["Leaf rust", "Stem Rust", "Healthy"]
 
