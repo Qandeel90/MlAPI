83128de8b4fd2bd933eafdbb7be2940f02708997
Qandeel90 <95974099+Qandeel90@users.noreply.github.com>
2022-03-27 15:13:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -5,7 +7,6 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-import requests
 
 app = FastAPI()
 
@@ -21,9 +22,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-endpoint = "models\1"
+MODEL = tf.keras.models.load_model("models\1")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES = ["Leaf Rust", "Stem Rust", "Healthy"]
 
 
 @app.get("/ping")
@@ -43,20 +44,14 @@ async def predict(
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
 
-    json_data = {
-        "instances": img_batch.tolist()
-    }
+    predictions = MODEL.predict(img_batch)
 
-    response = requests.post(endpoint, json=json_data)
-    prediction = np.array(response.json()["predictions"][0])
-
-    predicted_class = CLASS_NAMES[np.argmax(prediction)]
-    confidence = np.max(prediction)
-
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
     return {
-        "class": predicted_class,
-        "confidence": float(confidence)
+        'class': predicted_class,
+        'confidence': float(confidence)
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, debug=True)
